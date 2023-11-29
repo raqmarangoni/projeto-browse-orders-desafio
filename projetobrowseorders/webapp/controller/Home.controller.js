b5fc10d03c1d3203e2d72c84dcdb43a6c0c35269
raqmarangoni <rmarangonisan@gmail.com>
2023-11-28 22:42:40 -0300

    feat:search field

--- projetobrowseorders/webapp/controller/Home.controller.js
+++ projetobrowseorders/webapp/controller/Home.controller.js
@@ -1,21 +1,41 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageBox",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
     "com/lab2dev/projetobrowseorders/model/models"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageBox, models) {
+    function (Controller, MessageBox, Filter, FilterOperator, models) {
         "use strict";
 
         return Controller.extend("com.lab2dev.projetobrowseorders.controller.Home", {
+            
             onInit: function () {
-                const orders = models.getOrders();
-
+                const parameters = {
+                    urlParameters: {
+                        $expand:"Customer"
+                    }
+                }
+                const orders = models.getOrders(parameters);
                 orders
-                    .then(oOrdersModel => this.getView().setModel(oOrdersModel, 'orders'))
+                    .then(oOrdersModel => this.getView().setModel(oOrdersModel, "orders"))
                     .catch(oError => MessageBox.error(oError));
-            }
+            },
+
+            onSearch: function(oEvent) {
+                const aFilters = [];
+                const sQuery = oEvent.getSource().getValue();
+                if (sQuery && sQuery.length > 0) {
+                    const filter = new Filter("Customer/CompanyName", FilterOperator.Contains, sQuery);
+                    aFilters.push(filter);
+                };
+
+                const oList = this.byId("list");
+                const oBinding = oList.getBinding("items");
+                oBinding.filter(aFilters);
+            },
         });
     });
